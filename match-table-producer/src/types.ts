715a412f8dff4626ee2c3c33c45e298bb40b1789
Rory Malcolm <rorymckenziemalcolm@gmail.com>
2023-05-31 12:55:32 +0100

    match table producer

---
+++ match-table-producer/src/types.ts
@@ -0,0 +1,11 @@
+export type MatchForDb = {
+  id: string;
+  home: string;
+  away: string;
+  matchTime: string;
+  score: string;
+  homeScore: number;
+  awayScore: number;
+  homeXG: number;
+  awayXG: number;
+};
---
+++ match-table-producer/src/worker.ts
@@ -0,0 +1,67 @@
+import { MatchForDb } from './types';
+
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    const url = new URL(request.url);
+    if (url.pathname.includes('favicon')) {
+      return new Response(null, { status: 404 });
+    }
+    const resultsPromise = env.MATCH_STATS_DB.prepare('SELECT DISTINCT * FROM matches;').all<MatchForDb>();
+    ctx.waitUntil(resultsPromise);
+    const results = await resultsPromise;
+    console.log(results);
+    if (results.results === undefined || results.results.length === 0) {
+      return new Response('No results found');
+    }
+    return new Response(
+      placeResultsTableInValidHtmlDocument(
+        results.results.sort((a, b) => {
+          if (a.matchTime < b.matchTime) {
+            return -1;
+          }
+          if (a.matchTime > b.matchTime) {
+            return 1;
+          }
+          return 0;
+        })
+      ),
+      {
+        headers: {
+          'content-type': 'text/html;charset=UTF-8',
+        },
+      }
+    );
+  },
+};
+
+function placeResultsTableInValidHtmlDocument(results: MatchForDb[]): string {
+  return `<html><head><title>Match Results</title></head><body>${formatResultsInHtmlTable(results)}</body></html>`;
+}
+
+function formatResultsInHtmlTable(results: MatchForDb[]): string {
+  return `<table>${formatResultsInHtmlTableRows(results)}</table>`;
+}
+
+function formatResultsInHtmlTableRows(results: MatchForDb[]): string {
+  return `<tr>${formatResultsInHtmlTableHeaders(results)}</tr>${formatResultsInHtmlTableBody(results)}`;
+}
+
+function formatResultsInHtmlTableHeaders(results: MatchForDb[]): string {
+  return `<th>${Object.keys(results[0]).join('</th><th>')}</th>`;
+}
+
+function formatResultsInHtmlTableBody(results: MatchForDb[]): string {
+  return results.map(formatResultInHtmlTableRow).join('');
+}
+
+function formatResultInHtmlTableRow(result: MatchForDb): string {
+  return `<tr>${formatResultInHtmlTableRowCells(result)}</tr>`;
+}
+
+function formatResultInHtmlTableRowCells(result: MatchForDb): string {
+  return Object.values(result).map(formatResultInHtmlTableRowCell).join('');
+}
+
+function formatResultInHtmlTableRowCell(result: string | number): string {
+  return `<td>${result}</td>`;
+}
